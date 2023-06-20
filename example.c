fa80568823a106485d058b29f65ea41bbd1843c8
toiletbril <s9und@outlook.com>
2023-06-21 00:43:39 +0300

    Rename test.c to example.c

---
+++ example.c
@@ -0,0 +1,29 @@
+#define TOILETLINE_IMPL
+#include "toiletline.c"
+
+#define LINE_BUF_LEN 1024
+
+int main(void)
+{
+    if (!tl_init()) {
+        printf("Failed to enter raw mode!\n");
+    }
+
+    printf("Welcome to toiletline test! This is raw mode!\nUse up and down arrows to view history.\n");
+
+    char line_buffer[LINE_BUF_LEN];
+    int i = 0;
+
+    while (tl_readline(line_buffer, LINE_BUF_LEN) == 0) {
+        printf("\nReceived string: '%s'\n", line_buffer);
+        fflush(stdout);
+        if (i++ >= 10) {
+            printf("Reached 10 messages, exiting!\n");
+            break;
+        }
+    }
+
+    tl_exit();
+
+    return 0;
+}
