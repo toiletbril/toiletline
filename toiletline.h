254a6ea15a79ecd5b27b44ff6014ed4306815bcb
toiletbril <s9und@outlook.com>
2023-09-03 06:30:22 +0300

    Windows.h -> windows.h

--- toiletline.h
+++ toiletline.h
@@ -157,7 +157,7 @@ size_t tl_utf8_strlen(const char *utf8_str);
 
 #if defined(ITL_WIN32)
     #define WIN32_LEAN_AND_MEAN
-    #include <Windows.h>
+    #include <windows.h>
     #include <conio.h>
     #include <fcntl.h>
     #include <io.h>
