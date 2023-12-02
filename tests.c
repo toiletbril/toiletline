2c6c4d1537888f82d71fac2c53271ad1a03666e7
toiletbril <s9und@outlook.com>
2023-12-02 10:34:51 +0300

    Be more pedantic

--- tests.c
+++ tests.c
@@ -3,73 +3,71 @@
 
 #define OUT_BUFFER_SIZE 128
 
-int test_string_from_cstr()
+static bool test_string_from_cstr()
 {
     char original_cstr[] = "привет, мир";
-    itl_string_t *str = itl_string_alloc();
-    itl_string_from_cstr(str, original_cstr);
-
     char out_buffer[OUT_BUFFER_SIZE];
+    int result;
+
+    itl_string_t *str = itl_string_alloc();
+
+    itl_string_from_cstr(str, original_cstr);
     itl_string_to_cstr(str, out_buffer, OUT_BUFFER_SIZE);
 
-    int result = strcmp(out_buffer, original_cstr);
-
-    if (result) {
+    result = strcmp(out_buffer, original_cstr);
+    if (result == 0)
+        return true;
+    else {
         printf("*** Result: '%s', should be: '%s'\n",
                out_buffer, original_cstr);
+        return false;
     }
-
-    if (result == 0 && str->size == 20 && str->length == 11)
-        return 0;
-    else
-        return 1;
 }
 
-int test_string_shift_backward()
+static bool test_string_shift_backward()
 {
     char original_cstr[] = "hello world sailor";
+    char should_be[] = "hello sailor";
+    char out_buffer[OUT_BUFFER_SIZE];
+
     itl_string_t *str = itl_string_alloc();
     itl_string_from_cstr(str, original_cstr);
-
     itl_string_shift(str, 11, 7, 1);
-
-    char out_buffer[OUT_BUFFER_SIZE];
     itl_string_to_cstr(str, out_buffer, OUT_BUFFER_SIZE);
 
-    char should_be[] = "hello sailor";
     int result = strcmp(out_buffer, should_be);
-
-    if (result) {
+    if (result != 0) {
         printf("*** Result: '%s', should be: '%s'\n",
                out_buffer, should_be);
+        return false;
     }
 
-    return result;
+    return true;
 }
 
-int test_string_shift_forward()
+static bool test_string_shift_forward()
 {
     char original_cstr[] = "hello world 69ilor";
-    itl_string_t *str = itl_string_alloc();
-    itl_string_from_cstr(str, original_cstr);
-
-    itl_string_shift(str, 11, 2, 0);
-
+    char should_be[] = "hello world 6969ilor";
     char out_buffer[OUT_BUFFER_SIZE];
+
+    itl_string_t *str = itl_string_alloc();
+
+    itl_string_from_cstr(str, original_cstr);
+    itl_string_shift(str, 11, 2, 0);
     itl_string_to_cstr(str, out_buffer, OUT_BUFFER_SIZE);
 
-    char should_be[] = "hello world 6969ilor";
     int result = strcmp(out_buffer, should_be);
-
-    if (result) {
+    if (result != 0) {
         printf("*** Result: '%s', should be: '%s'\n",
                out_buffer, should_be);
+        return false;
     }
 
-    return result;
+    return true;
 }
 
-typedef int (*test_func)(void);
+typedef bool (*test_func)(void);
 
 typedef struct test_case test_case_t;
 
@@ -95,12 +93,17 @@ test_case_t test_cases[] = {
     (sizeof(test_cases)/sizeof(test_cases[0]))
 
 int main(void) {
-    for (size_t i = 0; i < TEST_CASES_COUNT; ++i) {
-        int result = test_cases[i].func();
+    bool result;
+    size_t i;
 
-        printf("> Test #%zu '%s' ", i, test_cases[i].name);
-        if (result) printf("failed.\n");
-        else printf("passed.\n");
+    for (i = 0; i < TEST_CASES_COUNT; ++i) {
+        result = test_cases[i].func();
+
+        printf("> Test #%zu '%s'", i, test_cases[i].name);
+        if (!result)
+            printf("failed.\n");
+        else
+            printf("...");
     }
 
     return 0;
